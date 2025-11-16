fb2e5281dd86d50496f03b158e46322449b2db26
Adrian11126 <ae230301@gmail.com>
2025-11-16 17:34:11 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,36 @@
-"""
+
 calculator.py
-- Defines functions used to create a simple calculator
+- Defines functions used to create a simple calculator.
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+
+# Addition
+def add(a, b):
+    return a + b
+
+# Subtraction
+def subtract(a, b):
+    return a - b
+
+# Multiplication
+def multiply(a, b):
+    return a * b
+
+# Division
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
+
+# Exponent
+def power(a, b):
+    return a ** b
+
+# Modulo
+def mod(a, b):
+    if b == 0:
+        raise ValueError("Cannot modulo by zero.")
+    return a % b
 
 
